65a24439b1d6479cff97ec65bcd188e4785ee2da
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-09-24 11:01:09 +0200

    feat: ajout de la propriété username dans l'entité User
    + migration
    + modification du formType RegistrationFormType

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,6 +16,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
     #[ORM\Column(length: 180)]
     private ?string $email = null;
 
@@ -128,6 +131,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
 
         return $this;
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
+        ->add('username', TextType::class, [
+            "label" => "Pseudo",
+        ])
+            ->add('email', EmailType::class, [
+                "label" => "Email",
+            ])
             ->add('plainPassword', RepeatedType::class, [
                 "type" => PasswordType::class,
                 'first_options' => ['label' => 'Mot de passe'],
